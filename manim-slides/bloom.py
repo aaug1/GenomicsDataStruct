64766e002150c90aff2eea6c4860a2717329fb76
Aidan Aug <aidanaug123@gmail.com>
2022-11-15 21:10:25 -0500

    Adds title slide to animation

--- manim-slides/bloom.py
+++ manim-slides/bloom.py
@@ -2,46 +2,83 @@ from manim import *
 # or: from manimlib import *
 from manim_slides import Slide
 from manim.mobject.types.vectorized_mobject import VGroup, VMobject
-import random
 from BloomFilter import BloomFilter
+import random
 
 
 class Bloom(Slide):
     def construct(self):
-        # Add a plane for visualization
-        # plane = NumberPlane(background_line_style={
-        #     "stroke_color": TEAL,
-        #     "stroke_width": 4,
-        #     "stroke_opacity": 0.6
-        # })
-        # self.add(plane)
         ## Define input parameters
         
-        array_len = 10
+        array_len = 15
         num_hash = 5
-        sample_keys = ["Ben", "Langmead"]
-        non_keys = ["Aidan", "Aug"]
-        box_size = array_len / (array_len + 5)
-        bloom_filter = BloomFilter(num_hash = num_hash, size=array_len)
-
-        # Slide 1: Initialize array of zeros
+        sample_keys = ["ACTGA", "Langmead"]
+        non_keys = ["ACTGG", "GCTGA", "Ben"]
+        box_size = 8 / (array_len)
+        bloom_filter = BloomFilter(num_hash = num_hash, size = array_len)
+        
+        # Create the array
         array = Array(array_len=array_len, box_size=box_size, fill_color=DARK_GRAY, color=LIGHT_GRAY, indices=True)
-        array.set_indices()
+        array.shift(ORIGIN, DOWN).scale(2)
+        squares = array.get_squares()
+        
+        ################# Title: #################
+        bloom_flower = VGroup(array)
+        hash = []
+        for i in range(num_hash):
+            mathText = MathTex(f"h{i + 1}")
+            mathText.shift(array.get_center())
+            hash.append(mathText)
 
+        for i, hash_mobj in enumerate(hash):
+            hash_mobj.shift(RIGHT*(i-num_hash//2) * 0.5 + UP*4)
+            
+        hash_indices = np.arange(num_hash)
+        np.random.shuffle(hash_indices)
+        all_lines = VGroup()
+        for i, square in enumerate(squares):
+            p1 = square.get_center()
+            p1b = p1 + [0, 0.75, 0]
+            d1 = Dot(point=p1).set_color(BLUE)
+            l1 = Line(p1b, p1)
+            p2 = hash[hash_indices[i% num_hash]].get_center()
+            p2b = p2 - [0, 0.75, 0]
+            d2 = Dot(point=p2).set_color(RED)
+            l2 = Line(p2, p2b)
+            bezier = CubicBezier(p2b,p2b - UP, p1b + UP, p1b)
+            total = VGroup(d2, l2, bezier, l1, d1)
+            total.set_color(random_bright_color())
+            all_lines.add(total)
+            bloom_flower.add(d1, d2, total)
+        bloom_flower.scale(0.75).shift(DOWN * 2)
+
+        
+        title = Title(f"Bloom Filter")
+        subtitle1 = Text(f"Computational Genomics: Final Project", slant=ITALIC).next_to(title, DOWN)
+        subtitle2 = Text(f"Team 42", slant=ITALIC).next_to(subtitle1, DOWN)
+        subtitle3 = Text(f"Aidan Aug, Karen He, Mark Tiavises, Alan Zhang", slant=ITALIC).next_to(subtitle2, DOWN)
+        self.play(Write(title), run_time=1)
+        self.play(Write(subtitle1.scale(0.6)), run_time=0.5)
+        self.play(Write(subtitle2.scale(0.5)), Write(subtitle3.scale(0.5)), run_time=0.5)
+        title_text = VGroup(title, subtitle1, subtitle2, subtitle3)
+
+        self.play(Write(bloom_flower), run_time=2)
+        self.add(bloom_flower)
+        self.pause()
+        self.play(Unwrite(all_lines), Unwrite(title_text))
+
+        ############### Slide 1: Initialize array of zeros ###############
         # Create Pseudocode Block
         self.arrow = Arrow(buff=0.2, start=ORIGIN, end=RIGHT, stroke_width=8, max_stroke_width_to_length_ratio=10)
         self.create_function()
-
+        
+        array.set_indices()
         self.slide1(array)
         self.highlight(self.code_lines, 0, 1)
         
-        ## Slide 2: Shift and move hash functions above the array
-        hash = []
-        for i in range(num_hash):
-            mathText = MathTex(f"h{i}")
-            mathText.shift(array.get_center() + RIGHT*(i-num_hash//2) + UP*2)
-            hash.append(mathText)
+        
 
+        ## Slide 2: Shift and move hash functions above the array
         self.slide2(array, hash, num_hash)
         self.highlight(self.code_lines, 1, 2)
 
@@ -49,7 +86,7 @@ class Bloom(Slide):
         self.slide3(array, num_hash, hash, sample_keys, bloom_filter)
 
         self.highlight(self.code_lines, 2, 3)
-        #self.slide4(array, num_hash, hash, sample_keys, bloom_filter)
+        self.slide4(array, num_hash, hash, sample_keys, bloom_filter)
 
         self.highlight(self.code_lines, 3, 4)
         self.slide5(array, num_hash, hash, non_keys, bloom_filter)
@@ -92,7 +129,7 @@ class Bloom(Slide):
 
 
     def slide1(self, array):
-        self.play(DrawBorderThenFill(array), array.animate.shift(DOWN*2).shift(LEFT*2).scale(0.7), run_time=1)
+        self.play(array.animate.shift(UP*0.5).shift(LEFT*2).scale(0.7), run_time=1)
         self.pause() # Waits user to press continue to go to the next slide
     
     #Shift
@@ -100,7 +137,10 @@ class Bloom(Slide):
         #all_hash = VGroup()
             #all_hash.add(mathText)
         #all_hash.add_updater(lambda x: x.move_to(array.get_center()+UP*2))
-        all_hash = VGroup(*hash).add_updater(lambda x: x.move_to(array.get_center()+UP*2))
+        #all_hash = VGroup(*hash).add_updater(lambda x: x.move_to(array.get_center()+UP*2))
+        for i, item in enumerate(hash):
+          item.shift(array.get_center() + RIGHT * (i - num_hash//2))
+        all_hash = VGroup(*hash)
         self.play(FadeIn(all_hash))
         self.pause()
             
@@ -249,12 +289,59 @@ class Array(VMobject):
     def get_indices(self):
         return self.nums
 
+class CircularArray(VMobject):
+    def __init__(self, array_len, box_size, color=WHITE, indices=False, **kwargs):
+        # Set properties
+        self.array_len = array_len
+        self.box_size = box_size
+        self.squares = []
+        self.nums= []
+        VMobject.__init__(self, **kwargs)
 
-class CodeFromString(Scene):
-    def construct(self, input, non_input):
-        code = f'''import BloomFilter
+        circle_outer = Circle(radius=box_size * 3)
+        circle_innter = Circle(radius=box_size * 2)
+        
+        # Create the squares in circular array
+        for i in range(array_len):
+            square1 = Square(side_length=box_size, color=color).next_to(
+                square, RIGHT, buff=0).set_style(fill_color=DARK_GRAY, fill_opacity=1)
+            square = square1
+            self.add(square)
+            self.squares.append(square)
+    
+        self.center()
 
-'''
+
+    def get_squares(self):
+        return self.squares
+    
+    # Add the indices
+    def set_indices(self):
+        next_num = Integer()
+        # Fill indices with value
+        for i in range(self.array_len):
+            next_num = Integer().move_to(self.squares[i].get_center())
+            next_num.move_to(self.squares[i].get_center())
+            self.nums.append(next_num)
+            self.add(next_num)
+    
+    def get_indices(self):
+        return self.nums
+
+
+
+
+
+
+
+
+
+
+
+
+################# Additional #################
+
+
 
 class CodeFromString():
     def construct(self, input, non_input):
