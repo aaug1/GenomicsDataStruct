ed09eb18f3835208e981cdc7df4c98eaf8462438
Mark Tiavises <mtiavis1@jhu.edu>
2022-11-27 16:07:46 -0500

    Three slides done for cuckoo

--- manim-slides/cuckoo.py
+++ manim-slides/cuckoo.py
@@ -1,18 +1,41 @@
-from manim import *
+from manim import Scene, Write, Text, FadeIn, FadeOut, ImageMobject, UP, DOWN, LEFT, RIGHT, Ellipse, Rectangle, VGroup, DrawBorderThenFill, BLUE_D, BLUE_B, BLUE, ORANGE
 
 
 class CreateCircle(Scene):
     def construct(self):
-      w = self.WriteQuestion()
-      self.play(w)
-      self.remove(w)
-      self.play(self.PoseMotivation())
-    
+        self.WriteQuestion()
+        self.PoseMotivation()
+        self.ListGoals()
+
     def WriteQuestion(self):
-      text = "What is a 🐔 filter?"
-      return Write(Text(text, font_size=100))
-    
+        text = Text("What is a 🐔 filter?", font_size=100)
+        self.play(Write(text))
+        self.play(FadeOut(text))
+
     def PoseMotivation(self):
-      text = "2nd slide please"
-      return Write(Text(text, font_size=100))
-      
+        kirby = ImageMobject("images/8-bit-kirby.png")
+        food_pile = ImageMobject("images/8-bit-food-pile.jpg")
+        cherry_text = Text("I hate cherries", font_size=30)
+        bubble = Ellipse(width=3.0, height=2.0, color=BLUE_D)
+        bubble.set_fill(BLUE, opacity=0.5)
+        speech_bubble = VGroup()
+        speech_bubble.add(bubble, cherry_text).shift(5*LEFT).shift(0.5 * UP)
+        self.play(FadeIn(kirby))
+        self.play(kirby.animate.shift(5*LEFT).shift(2*DOWN))
+        self.play(FadeIn(food_pile))
+        self.wait(5)
+        self.play(FadeIn(speech_bubble))
+        self.wait(3)
+        self.play(FadeOut(speech_bubble))
+        self.play(FadeOut(kirby))
+        self.play(FadeOut(food_pile))
+
+    def ListGoals(self):
+        background = Rectangle(
+            height=3, width=6, fill_opacity=1, fill_color=BLUE)
+        goals = Text('First_line\nSecond_line\nThird_line')
+        background_goals = VGroup()
+        background_goals.add(background, goals)
+        self.play(DrawBorderThenFill(background))
+        self.play(Write(goals))
+        self.play(background_goals.animate.shift(4*LEFT).shift(2*UP))
