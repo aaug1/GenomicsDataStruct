83824c5866a9589ce947139797ac305936b3ecfc
mtiavis1 <mtiavis1@jhu.edu>
2022-12-04 00:22:59 -0500

    Finishing most of movie

--- manim-slides/cuckoo.py
+++ manim-slides/cuckoo.py
@@ -1,14 +1,18 @@
-from manim import MathTex, Scene, Write, Text, FadeIn, FadeOut, ImageMobject, UP, DOWN, LEFT, RIGHT, UL, UR, DL, DR, Ellipse, Rectangle, Circle, Line, Arrow, GrowArrow, ArrowSquareTip, Group, VGroup, Circumscribe, DrawBorderThenFill, BLUE_D, BLUE_B, BLUE, ORANGE, DARK_GRAY, LIGHT_GRAY, WHITE, VMobject, Square
-
+from manim import MathTex, Scene, Write, Text, FadeIn, FadeOut, ImageMobject, ArcBetweenPoints, UP, DOWN, LEFT, RIGHT, UL, UR, DL, DR, Ellipse, Rectangle, Circle, Line, Arrow, CurvedArrow, GrowArrow, ArrowSquareTip, Group, VGroup, Circumscribe, DrawBorderThenFill, BLUE_D, BLUE_B, BLUE, ORANGE, DARK_GRAY, LIGHT_GRAY, WHITE, VMobject, Square
+import numpy as np
 
 class CuckooFilter(Scene):
     def construct(self):
-        # self.WriteQuestion()
-        # self.PoseMotivation()
-        # self.ListGoals()
-        # self.MakeDataStructure()
-        # self.ExplainFinger()
+        self.WriteQuestion()
+        self.PoseMotivation()
+        self.ListGoals()
+        self.MakeDataStructure()
+        self.ExplainFinger()
+        self.Cycles()
         self.ExplainHashes()
+        self.LookUpAndDelete()
+        self.FalsePositive()
+        
 
     def WriteQuestion(self):
         text = Text("What is a 🐔 filter?", font_size=100)
@@ -70,7 +74,7 @@ class CuckooFilter(Scene):
 
         middle_group = VGroup()
         for i in range(0, 8, 1):
-            middle_group += Arrow(max_stroke_width_to_length_ratio=5).shift(i*.8 * DOWN)
+            middle_group += Arrow(max_stroke_width_to_length_ratio=5).shift(i*.8 * DOWN + .2*RIGHT)
 
         numEntries = 2
         cuckooFilter = Group()
@@ -219,7 +223,7 @@ class CuckooFilter(Scene):
 
     def ExplainHashes(self):
         h1 = MathTex("h1( ) = hash(input object)").shift(4*LEFT+3*UP)
-        h2 = MathTex("h0( ) = h0( ) \oplus hash(fingerprint)").shift(
+        h2 = MathTex("h0( ) = h1( ) \oplus hash(fingerprint)").shift(
             3.2*LEFT+2*UP)
         self.play(FadeIn(h1))
         self.play(FadeIn(h2))
@@ -241,7 +245,203 @@ class CuckooFilter(Scene):
 
         h2_val = MathTex("h0( ) = 01001").shift(5.4*LEFT + DOWN)
         self.play(FadeIn(h2_val))
+        
+        self.play(FadeOut(h1, h2, bit_two, bit_one, l, bit_three))
+        move_group = VGroup(h1_val, hf_val, h2_val)
+        self.play(move_group.animate.shift(2*UP))
+        
+        cur_index_num = MathTex("firstIndex = h0( ) = 01001").shift(1*UP)
+        first_index = Square(side_length=1.5)
+        fingerprint_num = MathTex("010")
+        orig_storage = VGroup(cur_index_num, first_index, fingerprint_num).shift(4*RIGHT + 2*UP)
+        self.play(FadeIn(orig_storage))
+        
+        self.play(Circumscribe(cur_index_num, shape=Rectangle))
+        self.play(Circumscribe(hf_val, shape=Rectangle))
+        
+        newMath = VGroup(bit_three.shift(.5*UP), bit_two, l.shift(.5*DOWN), bit_one.shift(2*DOWN)).shift(2.5*DOWN + 11*LEFT)
+        self.play(FadeIn(newMath))
+        
+        self.play(Circumscribe(bit_one), shape=Rectangle)
+        self.play(Circumscribe(h1_val), shape=Rectangle)
+        
+        self.play(FadeOut(Group(*self.mobjects)))
+    
+    def LookUpAndDelete(self):
+        function_title = Text("Perform Lookup", font_size=40).shift(3.5*UP + 4*LEFT)
+        self.play(Write(function_title))
+        
+        self.MakeDataStructure(play_cherry=False)
+        first_bit_one = MathTex("000")
+        first_bit_two = MathTex("001").shift(.75*DOWN)
+        first_bit_three = MathTex("110").shift(1.5*DOWN)
+        firstEntry = VGroup(first_bit_one, first_bit_two, first_bit_three).shift(2.8*UP + 2*RIGHT)
+        self.play(FadeIn(firstEntry))
+        
+        second_bit_one = MathTex("111")
+        second_bit_two = MathTex("101").shift(.75*DOWN)
+        second_bit_three = MathTex("110").shift(1.5*DOWN)
+        secondEntry = VGroup(second_bit_one, second_bit_two, second_bit_three).shift(2.8*UP + 4.2*RIGHT)
+        self.play(FadeIn(secondEntry))
+        
+        cherry_finger = MathTex("111").shift(5.5*LEFT + 1.8*UP)
+        cherry = ImageMobject(
+            "images/8-bit-cherry.png").scale(.4).shift(5.3*LEFT + .5*UP)
+        self.play(FadeIn(cherry))
+        self.play(FadeIn(cherry_finger))
+        
+        all_hash = VGroup()
+        for i in range(1, -1, -1):
+            mathText = MathTex(f"h{i}")
+            mathText.shift(UP*(i*1.2))
+            all_hash.add(mathText)
+        self.play(FadeIn(all_hash.shift(LEFT*2)))
+        
+        h1Arr = Arrow(LEFT, RIGHT).shift(1.2*UP + 3.5*LEFT)
+        h2Arr = Arrow(LEFT, RIGHT).shift(3.5*LEFT)
+        self.play(GrowArrow(h1Arr))
+        self.play(GrowArrow(h2Arr))
 
+        index1Arr = Arrow(LEFT, 2*UR).shift(1.2*UP)
+        index2Arr = Arrow(LEFT, 1.5*UP+2*RIGHT).shift(.3*DOWN)
+        self.play(GrowArrow(index1Arr.shift(.1*DOWN + .3*LEFT)))
+        self.play(GrowArrow(index2Arr.shift(.2*UP + .3*LEFT)))
+        
+        self.play(Circumscribe(cherry_finger), shape=Rectangle)
+        self.play(Circumscribe(first_bit_three), shape=Rectangle)
+        self.play(Circumscribe(second_bit_three), shape=Rectangle)
+        self.play(Circumscribe(first_bit_one), shape=Rectangle)
+        self.play(Circumscribe(second_bit_one), shape=Rectangle)
+        
+        self.play(FadeOut(function_title))
+        function_title = Text("Perform Deletion", font_size=40).shift(3.5*UP + 4*LEFT)
+        self.play(Write(function_title))
+        
+        self.play(FadeOut(cherry_finger))
+        cherry_finger = MathTex("110").shift(5.5*LEFT + 1.8*UP)
+        self.play(FadeIn(cherry_finger))
+        
+        self.play(Circumscribe(cherry_finger), shape=Rectangle)
+        self.play(Circumscribe(first_bit_one), shape=Rectangle)
+        self.play(Circumscribe(second_bit_one), shape=Rectangle)
+        self.play(Circumscribe(first_bit_three), shape=Rectangle)
+        self.play(Circumscribe(second_bit_three), shape=Rectangle)
+        
+        self.play(FadeOut(first_bit_three))
+        
+        apple_finger = MathTex("110").shift(5.5*LEFT + DOWN)
+        apple = ImageMobject(
+            "images/8-bit-apple.png").scale(.5).shift(5.3*LEFT + 2*DOWN)
+        self.play(FadeIn(apple_finger, apple))
+        
+        self.play(FadeOut(Group(*self.mobjects)))
+    
+    def FalsePositive(self):
+        fp_title = Text("False Positive (How Bad?)", font_size=40).shift(3.5*UP + 3.6*LEFT)
+        self.play(Write(fp_title))
+        
+        firstFinger = Text("1st Fingerprint:", font_size=20)
+        onebitfinger = Text("1", font_size=40).shift(1.5*RIGHT)
+        combinedOne = VGroup(firstFinger, onebitfinger)
+        self.play(Write(combinedOne))
+        
+        self.play(combinedOne.animate.shift(2.5*UL + 2.5*LEFT))
+        
+        secondFinger = Text("2nd Fingerprint:", font_size=20)
+        secondonebitfinger = Text("1", font_size=40).shift(1.5*RIGHT)
+        combinedSecond = VGroup(secondFinger, secondonebitfinger)
+        self.play(Write(combinedSecond))
+        
+        self.play(combinedSecond.animate.shift(2.5*UL + 2.5*LEFT + .5*DOWN))
+        
+        prob_tex = Text("Probablity:", font_size=40)
+        probability = MathTex("1 \over 2").shift(2*RIGHT)
+        prob_sen = VGroup(prob_tex, probability)
+        self.play(Write(prob_sen))
+        self.play(prob_sen.animate.shift(2.5*UR + 1.5*RIGHT + .3*DOWN))
+        
+        self.play(FadeOut(onebitfinger, secondonebitfinger))
+        onebitfinger = Text("10", font_size=40).shift(1.5*RIGHT + 2.5*UL + 2.5*LEFT)
+        secondonebitfinger = Text("10", font_size=40).shift(1.5*RIGHT + 2.5*UL + 2.5*LEFT + .5*DOWN)
+        self.play(FadeIn(onebitfinger, secondonebitfinger))
+        
+        self.play(FadeOut(probability))
+        probability = MathTex("{1 \over 2}^2").shift(2*RIGHT + 2.5*UR + 1.5*RIGHT + .3*DOWN)
+        self.play(FadeIn(probability))
+        
+        
+        self.play(FadeOut(onebitfinger, secondonebitfinger, firstFinger, secondFinger))
+        self.play(FadeOut(probability))
+        probability = MathTex("({1 \over 2})^f").shift(2*RIGHT + 2.5*UR + 1.5*RIGHT + .3*DOWN)
+        self.play(FadeIn(probability))
+        
+        self.play(FadeOut(prob_tex))
+        prob_tex = Text("Probablity:", font_size=40).shift(LEFT + 2.5*UR + 1.5*RIGHT + .3*DOWN)
+        self.play(FadeOut(probability))
+        probability = MathTex("{2 \over m} \cdot ({1 \over 2})^f").shift(1.8*RIGHT + 2.5*UR + 1.5*RIGHT + .3*DOWN)
+        prob_sen = VGroup(prob_tex, probability)
+        self.play(FadeIn(prob_sen))
+        
+        self.play(FadeOut(Group(*self.mobjects)))
+        
+    def Cycles(self):
+        self.MakeDataStructure(play_cherry=False)
+        
+        first_bit_one = MathTex("000")
+        first_bit_two = MathTex("001").shift(.75*DOWN)
+        first_bit_three = MathTex("110").shift(1.5*DOWN)
+        firstEntry = VGroup(first_bit_one, first_bit_two, first_bit_three).shift(2.8*UP + 2*RIGHT)
+        self.play(FadeIn(firstEntry))
+        
+        second_bit_one = MathTex("111")
+        second_bit_two = MathTex("101").shift(.75*DOWN)
+        second_bit_three = MathTex("110").shift(1.5*DOWN)
+        secondEntry = VGroup(second_bit_one, second_bit_two, second_bit_three).shift(2.8*UP + 4.2*RIGHT)
+        self.play(FadeIn(secondEntry))
+        
+        cherry_finger = MathTex("111").shift(5.5*LEFT + 1.8*UP)
+        cherry = ImageMobject(
+            "images/8-bit-cherry.png").scale(.4).shift(5.3*LEFT + .5*UP)
+        self.play(FadeIn(cherry))
+        self.play(FadeIn(cherry_finger))
+        
+        all_hash = VGroup()
+        for i in range(1, -1, -1):
+            mathText = MathTex(f"h{i}")
+            mathText.shift(UP*(i*1.2))
+            all_hash.add(mathText)
+        self.play(FadeIn(all_hash.shift(LEFT*2)))
+        
+        h1Arr = Arrow(LEFT, RIGHT).shift(1.2*UP + 3.5*LEFT)
+        h2Arr = Arrow(LEFT, RIGHT).shift(3.5*LEFT)
+        self.play(GrowArrow(h1Arr))
+        self.play(GrowArrow(h2Arr))
+
+        index1Arr = Arrow(LEFT, 1.8*UR).shift(1.2*UP)
+        index2Arr = Arrow(LEFT, 1.5*UP+1.6*RIGHT).shift(.3*DOWN)
+        self.play(GrowArrow(index1Arr.shift(.1*DOWN + .3*LEFT)))
+        self.play(GrowArrow(index2Arr.shift(.2*UP + .3*LEFT)))
+        
+        self.play(Circumscribe(first_bit_three), shape=Rectangle)
+        self.play(Circumscribe(second_bit_three), shape=Rectangle)
+        self.play(Circumscribe(first_bit_one), shape=Rectangle)
+        self.play(Circumscribe(second_bit_one), shape=Rectangle)
+        
+        #self.play(FadeOut(index1Arr, index2Arr))
+        
+        firstCyc = ArcBetweenPoints(start=UP + RIGHT, end=DOWN + RIGHT, radius=2, angle=90).shift(2*UP + .5*RIGHT)
+        firstCyc.add_tip()
+        self.play(FadeIn(firstCyc))
+        
+        secondCyc = ArcBetweenPoints(start=DOWN + LEFT, end=UP + LEFT, radius=2, angle=90).shift(2*UP + 5.8*RIGHT)
+        secondCyc.add_tip()
+        self.play(FadeIn(secondCyc))
+        
+        cycText = Text("max_cycles=200").shift(2*DOWN + 2*LEFT)
+        self.play(Write(cycText))
+        
+        self.play(FadeOut(Group(*self.mobjects)))
+        
 
 class Vert_Array(VMobject):
     def __init__(self, array_len, box_size, color=WHITE, indices=False, **kwargs):
