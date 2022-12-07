a0e9abfd727067522440b60ad55d9cf6ce3f653f
Aidan Aug <aidanaug123@gmail.com>
2022-12-07 15:57:12 -0500

    Formats countMin.py code

--- manim-slides/countMin.py
+++ manim-slides/countMin.py
@@ -2,9 +2,7 @@ from manim import *
 # or: from manimlib import *
 from manim_slides import Slide
 from manim.mobject.types.vectorized_mobject import VGroup, VMobject
-from BloomFilter import BloomFilter
 import random
-import math
 
 
 class CountMinIntro(Slide):
@@ -22,21 +20,7 @@ class CountMinIntro(Slide):
         array.shift(ORIGIN, DOWN * 1.5 + RIGHT * 4).scale(0.5)
         squares = array.get_squares()
 
-        ################# Title: #################
-        count_min = VGroup(array)
-        hash = []
-        
-
-        # Create text
-        for i in reversed(range(num_hash)):
-            mathText = MathTex(f"h{i + 1}")
-            mathText.shift(array.get_center())
-            hash.append(mathText)
-
-        for i, hash_mobj in enumerate(hash):
-            hash_mobj.next_to(array.get_left() + LEFT * 3 + (num_hash//2 - i) * DOWN)
-            count_min.add(hash_mobj)
-            
+        ################# Title #################
         # Text for title slide
         title = Title(f"Count Min Sketch")
         subtitle1 = Text(f"Computational Genomics: Final Project",
@@ -49,60 +33,13 @@ class CountMinIntro(Slide):
         self.play(Write(subtitle2.scale(0.5)), Write(
             subtitle3.scale(0.5)), run_time=0.5)
         title_text = VGroup(subtitle1, subtitle2, subtitle3)
-        self.play(Write(count_min))
 
-        # Generate the stream
-        self.start_loop()
+        # Generate introductory animation on title slide
+        count_min_intro, hash_text_group = self.count_min_intro_animation(array_len, num_hash, array)
+        self.play(Unwrite(title_text))
 
-        d1 = Dot().set_color(ORANGE)
-        l1 = Line(3 * LEFT, RIGHT).to_edge(LEFT).set_y(count_min.get_y())
-        l2 = VMobject()
-        self.add(d1, l1, l2)
-        l2.add_updater(lambda x: x.become(Line(l1.get_left(), d1.get_center()).set_color(ORANGE)))
-        self.play(MoveAlongPath(d1, l1), rate_func=linear)
-        
-        in_arrows_list = []
-        for i in range(num_hash):
-            arrow = Arrow(d1.get_center(), hash[i].get_center())
-            in_arrows_list.append(arrow)
-
-        out_arrows_list = []
-        for i in range(num_hash):
-            radius = 1 if i < i//2 else -1
-            rand_m = random.randint(0, array_len - 1)
-            arrow = CurvedArrow(hash[i].get_center(), squares[num_hash-i-1][rand_m].get_center(), radius=10*radius
-                                ).set_color_by_gradient(random_bright_color(), random_bright_color())
-            out_arrows_list.append(arrow)
-
-        animate_in_arrows = []
-        for i in (in_arrows_list):
-            animate_in_arrows.append(GrowArrow(i))
-        delete_in_arrows = []
-        for i in (in_arrows_list):
-            delete_in_arrows.append(Unwrite(i))
-
-        animate_out_arrows = []
-        for i in (out_arrows_list):
-            animate_out_arrows.append(Create(i))
-        delete_out_arrows = []
-        for i in (out_arrows_list):
-            delete_out_arrows.append(Unwrite(i))
-
-        self.play(*animate_in_arrows, run_time=1)
-        self.play(*delete_in_arrows, run_time=0.5)
-        self.play(*animate_out_arrows, run_time=1)
-        self.play(*delete_out_arrows, run_time=0.5)
-        self.remove(d1)
-    
-        
-        self.end_loop()
-        self.play(Unwrite(d1), Unwrite(l1), Unwrite(title_text))
-        self.pause()
-        self.wait()
-        
-
-        ############### Slide 1: Initialize array of zeros ###############
-        # # Create Pseudocode Block
+        ################# Generate the description + pseudocode #################
+        ## left-hand description
         blist1_title = Text("About").next_to(title, DOWN).scale(0.5)
         blist1 = BulletedList(
           "Probabilistic Data Structure", 
@@ -113,6 +50,7 @@ class CountMinIntro(Slide):
         self.play(Write(blist1_title))
         self.play(Write(blist1))
 
+        ## right-hand pseudocode
         blist2_title = Text("How to Use:").scale(0.5)
         blist2 = BulletedList(
           "Initialize 2D array of 0", 
@@ -123,16 +61,119 @@ class CountMinIntro(Slide):
         self.play(Write(blist2_title))
         self.play(Write(blist2))
         self.pause()
-
-        self.play(blist2.animate.set_color_by_tex("Initialize 2D array of 0", YELLOW), run_time=2)
+        
+        ############### Slide 1: Initialize array of zeros and show input ###############
+        self.play(blist2.animate.set_color_by_tex("Initialize 2D array of 0", YELLOW), run_time=1)
         array.init_val_zero()
         array.show_indices()
-
+        self.show_input(sample_keys, test_keys)
+        
+        ############### Slide 2: Pass in input into Count Min Sketch ###############
+        self.play(blist2.animate.set_color_by_tex("Initialize 2D array of 0", WHITE), run_time=1)
+        self.play(blist2.animate.set_color_by_tex("Pass input through each hash + map to indices", YELLOW), run_time=1)
+        self.slide2(sample_keys, count_min_intro, num_hash, hash_text_group, array)
+        
+        ############### Slide 3: Make space for query procedure description ###############
+        self.slide3(blist1_all, blist2_all, title)
+        self.play(blist2.animate.set_color_by_tex("Pass input through each hash + map to indices", WHITE), run_time=1)
+        self.play(blist2.animate.set_color_by_tex("Query: take min of mapped indices", YELLOW), run_time=1)
         self.pause()
 
+        ############### Slide 4: Query the test strings ###############
+        self.slide4(test_keys, count_min_intro, num_hash, hash_text_group, array)
+        self.pause()
+
+        # ################ Fade out everything ###############
+        self.play(
+            *[FadeOut(mob)for mob in self.mobjects]
+        )
+
+        ############### Conclusion ###############
+        self.conclusion()
+
+
+    def count_min_intro_animation(self, array_len, num_hash, array):
+      # Create the introductory animation        
+      count_min_intro = VGroup()
+      count_min_intro.add(array)
+
+      # Create and add hash function names
+      hash_text_group = []
+      for i in reversed(range(num_hash)):
+          mathText = MathTex(f"h{i + 1}")
+          mathText.shift(array.get_center())
+          hash_text_group.append(mathText)
+
+      for i, hash_mobj in enumerate(hash_text_group):
+          hash_mobj.next_to(array.get_left() + LEFT * 3 + (num_hash//2 - i) * DOWN)
+          count_min_intro.add(hash_mobj)
+
+      self.play(Write(count_min_intro))
+
+      # Generate the stream of data animation
+      self.start_loop()
+
+      d1 = Dot().set_color(ORANGE)
+      l1 = Line(3 * LEFT, RIGHT).to_edge(LEFT).set_y(count_min_intro.get_y())
+      l2 = VMobject()
+      self.add(d1, l1, l2)
+      l2.add_updater(lambda x: x.become(Line(l1.get_left(), d1.get_center()).set_color(ORANGE)))
+      self.play(MoveAlongPath(d1, l1), rate_func=linear)
+      
+      # Map orange dot to the hash functions
+      in_arrows_list = []
+      for i in range(num_hash):
+          arrow = Arrow(d1.get_center(), hash_text_group[i].get_center())
+          in_arrows_list.append(arrow)
+
+      # Map hash function values to indices in the 2D array
+      out_arrows_list = []
+      squares = array.get_squares()
+      for i in range(num_hash):
+          radius = 1 if i < i//2 else -1
+          rand_m = random.randint(0, array_len - 1)
+          arrow = CurvedArrow(hash_text_group[i].get_center(), squares[num_hash-i-1][rand_m].get_center(), radius=10*radius
+                              ).set_color_by_gradient(random_bright_color(), random_bright_color())
+          out_arrows_list.append(arrow)
+
+      # Group animations for simultaneous animation
+      animate_in_arrows = []
+      for i in (in_arrows_list):
+          animate_in_arrows.append(GrowArrow(i))
+      delete_in_arrows = []
+      for i in (in_arrows_list):
+          delete_in_arrows.append(Unwrite(i))
+
+      animate_out_arrows = []
+      for i in (out_arrows_list):
+          animate_out_arrows.append(Create(i))
+      delete_out_arrows = []
+      for i in (out_arrows_list):
+          delete_out_arrows.append(Unwrite(i))
+
+      # Play all animations
+      self.play(*animate_in_arrows, run_time=1)
+      self.play(*delete_in_arrows, run_time=0.5)
+      self.play(*animate_out_arrows, run_time=1)
+      self.play(*delete_out_arrows, run_time=0.5)
+      
+      self.remove(d1)
+      self.end_loop()
+      self.play(FadeOut(d1), FadeOut(l2), run_time=2)
+
+      self.pause()
+      self.wait(duration=1)
+
+      return count_min_intro, hash_text_group
+
+    # 1st slide: animate initial input and initialization of array
+    def slide1(self, array, sample_keys, test_keys):
+        array.init_val_zero()
+        array.show_indices()
         self.show_input(sample_keys, test_keys)
 
-        d1.move_to(l1.get_left())
+    # 2nd slide: Pass in input as data stream, mapping to indices
+    def slide2(self, sample_keys, count_min_intro, num_hash, hash_text_group, array):
         all_m_vals = [
           [0, 4, 9],
           [1, 3, 9],
@@ -141,24 +182,24 @@ class CountMinIntro(Slide):
           [1, 3, 9],
           [8, 2, 5]
           ]
-        self.play(blist2.animate.set_color_by_tex("Initialize 2D array of 0", WHITE), run_time=2)
-        self.play(blist2.animate.set_color_by_tex("Pass input through each hash + map to indices", YELLOW), run_time=2)
 
         for i, key in enumerate(sample_keys):
           key_mobj = Tex(key).scale(0.75)
-          key_mobj.add_updater(lambda x: x.move_to(d1.get_center() + UP * 0.5))
-          self.play(Write(key_mobj))
           d1 = Dot().set_color(ORANGE)
-          l1 = Line(3 * LEFT, RIGHT).to_edge(LEFT).set_y(count_min.get_y())
+          key_mobj.add_updater(lambda x: x.move_to(d1.get_center() + UP * 0.5))
+          
+          l1 = Line(3 * LEFT, RIGHT).to_edge(LEFT).set_y(count_min_intro.get_y()) # white line
           d1.move_to(l1.get_left())
-          l2 = VMobject()
+          self.play(Write(key_mobj))
+
+          l2 = VMobject() # orange line
           self.add(d1, l1, l2)
           l2.add_updater(lambda x: x.become(Line(l1.get_left(), d1.get_center()).set_color(ORANGE)))
           self.play(MoveAlongPath(d1, l1), rate_func=linear, run_time=3)
           self.play(Unwrite(key_mobj), run_time=0.5)
 
           ## Arrows
-          delete_out_arrows = self.map_arrows(num_hash, hash, d1, squares, all_m_vals[i], array)
+          delete_out_arrows = self.map_arrows(num_hash, hash_text_group, d1, all_m_vals[i], array)
           d1.move_to(l1.get_left())
           nums = array.get_values()
           update_vals = []
@@ -169,14 +210,11 @@ class CountMinIntro(Slide):
           self.play(*update_vals, run_time = 1)
           self.play(*delete_out_arrows, run_time=0.5)
 
-        # Slide 2: Make space for query procedure
-        self.pause()
+    # 3rd slide: Shift to make space for query procedure description
+    def slide3(self, blist1_all, blist2_all, title):
         self.play(Unwrite(blist1_all))
         self.play(blist2_all.animate.to_edge(LEFT))
-        self.wait()
-
-        self.play(blist2.animate.set_color_by_tex("Pass input through each hash + map to indices", WHITE), run_time=2)
-        self.play(blist2.animate.set_color_by_tex("Query: take min of mapped indices", YELLOW), run_time=2)
+        self.wait(duration=1)
 
         blist3_title = Text("Query:").scale(0.5)
         blist3 = BulletedList(
@@ -185,21 +223,22 @@ class CountMinIntro(Slide):
           height=2, width=5).next_to(blist3_title, DOWN)
         blist3_all = VGroup(blist3_title, blist3).next_to(title, DOWN).to_edge(RIGHT)
         self.play(Write(blist3_all))
-        self.pause()
 
+    # 4th slide: Query the strings in test_keys
+    def slide4(self, test_keys, count_min_intro, num_hash, hash_text_group, array):
+        squares = array.get_squares()
         all_m_vals = [
           [0, 4, 9],
           [1, 3, 9],
           [5, 2, 8],
           ]
-         # Slide 3: Query the query keys
         for i, key in enumerate(test_keys):
           key_mobj = Tex(key).scale(0.75)
+          d1 = Dot().set_color(ORANGE)
+          l1 = Line(3 * LEFT, RIGHT).to_edge(LEFT).set_y(count_min_intro.get_y())
+          d1.move_to(l1.get_left())
           key_mobj.add_updater(lambda x: x.move_to(d1.get_center() + UP * 0.5))
           self.play(Write(key_mobj))
-          d1 = Dot().set_color(ORANGE)
-          l1 = Line(3 * LEFT, RIGHT).to_edge(LEFT).set_y(count_min.get_y())
-          d1.move_to(l1.get_left())
           l2 = VMobject()
           self.add(d1, l1, l2)
           l2.add_updater(lambda x: x.become(Line(l1.get_left(), d1.get_center()).set_color(ORANGE)))
@@ -207,7 +246,7 @@ class CountMinIntro(Slide):
           self.play(Unwrite(key_mobj), run_time=0.5)
 
           ## Arrows
-          delete_out_arrows = self.map_arrows(num_hash, hash, d1, squares, all_m_vals[i], array)
+          delete_out_arrows = self.map_arrows(num_hash, hash_text_group, d1, all_m_vals[i], array)
           d1.move_to(l1.get_left())
           nums = array.get_values()
           update_vals = []
@@ -222,23 +261,11 @@ class CountMinIntro(Slide):
           # Display the equation underneath underneath
           min_val = MathTex(f'\min ( {",".join(all_hash_vals)} ) = {min(all_hash_vals)}').next_to(array, DOWN)
           self.play(Write(min_val))
-          self.wait()
+          self.wait(duration=1)
           self.play(Unwrite(min_val))
 
-        self.pause()
-        ################ Fade out everything ###############
-        self.play(
-            *[FadeOut(mob)for mob in self.mobjects]
-        )
-        ## Conclusion
-        self.conclusion()
-
-
-          
-
-
+    # Text for conclusion slide
     def conclusion(self):
-        # Text for conclusion slide
         title = Text(f"Thank you!")
         subtitle1 = Text(f"Computational Genomics: Final Project",
                          slant=ITALIC).next_to(title, DOWN)
@@ -250,9 +277,9 @@ class CountMinIntro(Slide):
         self.play(Write(subtitle2.scale(0.5)), Write(
             subtitle3.scale(0.5)), run_time=0.5)
 
-
-          
-    def map_arrows(self, num_hash, hash, d1, squares, m_vals, array):
+    # Maps arrows to indices in array          
+    def map_arrows(self, num_hash, hash, d1, m_vals, array):
+        squares = array.get_squares()
         in_arrows_list = []
         for i in range(num_hash):
             arrow = Arrow(d1.get_center(), hash[i].get_center())
@@ -286,8 +313,7 @@ class CountMinIntro(Slide):
 
         return delete_out_arrows
 
-        
-
+      
     # Animates the input strings and output strings
     def show_input(self, sample_keys, test_keys):
         input_string = "Input keys:\n["
@@ -318,23 +344,8 @@ class CountMinIntro(Slide):
         all_input = VGroup(t1, t2).to_corner(LEFT + DOWN)
         self.play(Write(all_input))
 
-    # Moves the yellow arrow in the function block from create_function
 
-    def conclusion(self):
-        # Text for conclusion slide
-        title = Text(f"Thank you!")
-        subtitle1 = Text(f"Computational Genomics: Final Project",
-                         slant=ITALIC).next_to(title, DOWN)
-        subtitle2 = Text(f"Team 42", slant=ITALIC).next_to(subtitle1, DOWN)
-        subtitle3 = Text(f"Aidan Aug, Karen He, Mark Tiavises, Alan Zhang",
-                         slant=ITALIC).next_to(subtitle2, DOWN)
-        self.play(Write(title), run_time=1)
-        self.play(Write(subtitle1.scale(0.6)), run_time=0.5)
-        self.play(Write(subtitle2.scale(0.5)), Write(
-            subtitle3.scale(0.5)), run_time=0.5)
-
-
-# Creates an object that looks like an array
+# Creates a MObject that looks like a 2D array
 class TwoDArray(VMobject):
     def __init__(self, array_len, num_hash, box_size, color=WHITE, indices=False, **kwargs):
         # Set properties
