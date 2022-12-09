52d082c1a23c8051d7f9d6cee1c32d545d377366
mtiavis1 <mtiavis1@jhu.edu>
2022-12-08 19:23:48 -0500

    Fix for false positive cuckoo

--- prob-data-struct/CuckooFilter/cuckooFilter.py
+++ prob-data-struct/CuckooFilter/cuckooFilter.py
@@ -1,6 +1,7 @@
 import hashlib
 import math
 import random
+from bitstring import BitArray
 
 # Sources used
 # https://www.pdl.cmu.edu/PDL-FTP/FS/cuckoo-conext2014.pdf
@@ -10,8 +11,8 @@ import random
 
 class CuckooFilter:
   # n = len(items), fp = false positive rate
-    def __init__(self, n: int, fp: float):
-        self.b = 4  # entries per bucket
+    def __init__(self, n: int, entries: int, fp: float):
+        self.b = entries  # entries per bucket
         self.max_kicks = 200  # number of kicks to try before giving up and declaring cycle
         # How long a fingerprint in bytes
         self.f = self.fingerprintLength(self.b, fp)
@@ -22,6 +23,7 @@ class CuckooFilter:
 
     def insert(self, input: str):
         i1, i2, f = self.hashes(input)
+        print(f)
         capped_i1 = i1 % self.m
         capped_i2 = i2 % self.m
         # The modulo self.m is to cap the hash so it can be used as an actual index
@@ -78,16 +80,30 @@ class CuckooFilter:
         raise Exception("Failue: Hashtable is full")
 
     def hashes(self, val: str):
-        h = self.sha_hash(bytes(val, 'utf-8'))
+        byte_val = self.sha_hash(bytes(val, 'utf-8'))
+
+        bit_string = ''
+        for b in byte_val:
+            if len(bit_string) == self.f:
+                break
+            for i in range(8):
+                if (b >> i) & 1:
+                    bit_string += '1'
+                else:
+                    bit_string += '0'
+                if len(bit_string) == self.f:
+                    break
         # We want to insert the finger print, not the value
-        fingerprint = h[0: self.f]
-        i1 = int.from_bytes(h, "big")
-        hash_f = int.from_bytes(self.sha_hash(fingerprint), "big")
+        fingerprint = bit_string
+        i1 = int.from_bytes(byte_val, "big")
+        hash_f = int.from_bytes(self.sha_hash(
+            bytes(fingerprint, 'utf-8')), "big")
         # xor because I no longer need to rely on the value string to calculate a second hash in collisions.
         # I can get an alternate by xoring i1 and hash of a finger print to get the 2nd index later on
         i2 = i1 ^ hash_f
         # returning the really big indicies and the fingerprint to insert
         # the modulo self.m is to cap the i1 nad i2 to be indicies of the actual buckets array
+
         return i1, i2, fingerprint
 
     # val must already be in bytes
@@ -119,8 +135,9 @@ class CuckooFilter:
 
 
 # Initialize the cuckoo filter,
-# need to give it how many entries you want in a bucket and the desired false positive rate
-x = CuckooFilter(5, .05)
+# need to give it how many items you plan to insert, the number of entries (how many columns), and the desired false positive rate
+# It will try to optimize the size for the table even if you give a small number of columns by increasing rows
+x = CuckooFilter(10, 2, .9)
 
 x.insert("AA")
 x.insert("AC")
@@ -152,20 +169,3 @@ print(x.lookup("TA"))
 print(x.lookup("TC"))
 print(x.lookup("TG"))
 print(x.lookup("TT"))
-
-# insert sequence of keys
-# x.insert("cherry")
-# print("inserted a cherry")
-# x.insert("pineapple")
-# print("inserted a pineapple")
-# x.insert("apple")
-# print("inserted an apple")
-
-# # # lookup those keys probably exist
-# print('lookup presence')
-# print(f'Is cherry in the filter: {str(print(x.lookup("cherry"))}')
-# print(f'Is pineapple in the filter: {str(print(x.lookup("pineapple"))}')
-# print(f'Is apple in the filter: {str(print(x.lookup("apple"))}')
-
-# # # lookup those keys do not exist
-# print(f'Is spoiled cherry in the filter: {str(print(x.lookup("spoiled cherry"))}')
